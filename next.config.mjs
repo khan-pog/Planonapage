7642da0a18faecc85b91b51033f7f33cf427c23e
Khan Thompson <khanthompson123@gmaill.com>
2025-06-10 13:39:49 +1000

    Update image domains in next.config.mjs to include www.poap.space
    - Added 'www.poap.space' to the list of allowed image domains for improved image handling.
    - Updated remote patterns to include 'www.poap.space' for enhanced image loading capabilities.

--- next.config.mjs
+++ next.config.mjs
@@ -7,12 +7,16 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ['images.unsplash.com', 'jixrmvs7efv07vtw.public.blob.vercel-storage.com'],
+    domains: ['images.unsplash.com', 'jixrmvs7efv07vtw.public.blob.vercel-storage.com', 'www.poap.space'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**.public.blob.vercel-storage.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.poap.space',
+      }
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
