c6414ff7a74f45a5a05dc40f1ade923107931c52
Khan Thompson <khanthompson123@gmaill.com>
2025-06-10 13:42:27 +1000

    Update image domain configuration in next.config.mjs to support wildcard subdomains
    - Changed the hostname from 'www.poap.space' to '**.poap.space' in the image domains configuration for broader image loading capabilities.

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'www.poap.space',
+        hostname: '**.poap.space',
       }
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
